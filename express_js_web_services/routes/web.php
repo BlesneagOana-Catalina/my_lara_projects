2dc8b100c180229841370204722950eb950ed14d
vagrant <vagrant@homestead>
2017-01-18 12:42:53 +0000

    Express js finished

--- express_js_web_services/routes/web.php
+++ express_js_web_services/routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+use GuzzleHttp\Psr7;
+Route::get('home', function(){
+    $client = new GuzzleHttp\Client();
+	$response = $client->get('{{your_ip}}:3000/api/users', [
+		'auth' => [
+			'oana', 
+			'parola'
+			]
+	]);
+$data=$response->getBody();
+$stream =Psr7\stream_for($data); 
+echo $stream;
+// string data
+echo $stream->read(3);
+// str
+echo "-------------------------------------------------------------------------------------------------";
+echo $stream->getContents();
+// ing data
+echo "-------------------------------------------------------------------------------------------------";
+var_export($stream->eof());
+// true
+echo "-------------------------------------------------------------------------------------------------";
+var_export($stream->tell());
+// 11
+echo "-------------------------------------------------------------------------------------------------";
+	
 });
